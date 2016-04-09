0cf3b831b6c2119c4df376819d0241dc28d73187
maestrow <andrey.shmelev@gmail.com>
2016-04-09 14:54:35 +0300

    Using multiple loggers

---
+++ HowTo.Log4Net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using log4net;
+
+namespace HowTo.Log4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ok");
+            Method1();
+            Method2();
+            Console.ReadKey();
+        }
+
+        static void Method1()
+        {
+            ILog log = LogManager.GetLogger("functional #1");
+            log.Debug(MethodBase.GetCurrentMethod().Name);
+        }
+
+        static void Method2()
+        {
+            ILog log = LogManager.GetLogger("functional #2");
+            log.Debug(MethodBase.GetCurrentMethod().Name);
+        }
+    }
+}
